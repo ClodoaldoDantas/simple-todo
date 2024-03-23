724b5663b1ecee0d0c6e68a082dabf1bd6c64df0
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-03-23 13:45:56 -0300

    Testes E2E da barra de progresso

--- cypress/e2e/todos.cy.ts
+++ cypress/e2e/todos.cy.ts
@@ -39,4 +39,18 @@ describe('Todos', () => {
 
     cy.get('#todo-list li').should('have.length', 0)
   })
+
+  it('should be able to display progress of completed tasks', () => {
+    cy.createTodos(['Beber café', 'Estudar para a prova', 'Fazer exercícios'])
+
+    cy.get("span[data-testid='task-progress']").should('have.text', '🌱 0%')
+
+    cy.get('#todo-list li').first().find('button[role="checkbox"]').click()
+    cy.get("span[data-testid='task-progress']").should('have.text', '🚀 33%')
+
+    cy.get('#todo-list li').eq(1).find('button[role="checkbox"]').click()
+    cy.get('#todo-list li').eq(2).find('button[role="checkbox"]').click()
+
+    cy.get("span[data-testid='task-progress']").should('have.text', '🎉 100%')
+  })
 })
